b8a5e821cc30b9497e9cf2a271f203ce897361b3
DJ Chen <80285637+dj707chen@users.noreply.github.com>
2023-11-14 22:26:11 -0600

    Save

--- migrations/example-changelog.sql
+++ migrations/example-changelog.sql
@@ -13,7 +13,7 @@ create table stock_values as
     with dates as (
         SELECT generate_series as date
         FROM generate_series('2020-01-01 00:00'::timestamp,
-                '2022-05-15 00:00', '24 hours')
+                '2020-01-15 00:00', '24 hours')
     )
     select stock, date, random() as stock_value
     from stocks cross join dates;
